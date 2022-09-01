9fbe73dc724b4140f4a65dc645d57bf8cabdf085
Wildan Shafa <63804246+wildanshf@users.noreply.github.com>
2022-09-01 19:01:07 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type student struct {
+	id     int64
+	name   string
+	street string
+	job    string
+	quotes string
+}
+
+func main() {
+	var allStudents = []student{
+		{
+			id:     1,
+			name:   "John",
+			street: "St. John",
+			job:    "Manager",
+			quotes: "I am a quote",
+		},
+		{
+			id:     2,
+			name:   "Budi",
+			street: "St. Budi",
+			job:    "Engineer",
+			quotes: "I am a quote",
+		},
+		{
+			id:     3,
+			name:   "Andi",
+			street: "St. Andi",
+			job:    "Doctor",
+			quotes: "I am a quote",
+		},
+		{
+			id:     4,
+			name:   "Ryan",
+			street: "St. Ryan",
+			job:    "Driver",
+			quotes: "I am a quote",
+		},
+	}
+
+	first, err := strconv.ParseInt(os.Args[1], 10, 64)
+
+	if err != nil {
+		fmt.Println("First input parameter must be integer")
+		os.Exit(1)
+	}
+
+	foundStudent, isError := findStudent(first, allStudents)
+
+	if isError != nil {
+		fmt.Println(isError)
+	} else {
+		fmt.Printf("%#v", foundStudent)
+	}
+}
+
+func findStudent(search int64, students []student) (student student, err error) {
+	for _, student := range students {
+		if student.id == search {
+			return student, nil
+		}
+	}
+	err = fmt.Errorf("student with id %d not found", search)
+	return student, err
+}
